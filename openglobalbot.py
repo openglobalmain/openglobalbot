bb4231c91b5fb9b5dea6c9914ed40b71c2c36e98
Danny_Ivanov <nokia5230dok@yandex.ru>
2022-11-27 03:30:06 +0700

    Update openglobalbot.py

--- openglobalbot.py
+++ openglobalbot.py
@@ -1,7 +1,8 @@
 import telebot
-token='5554332230:AAFnh_HngpQesOHui_kEZGSjJ0Vcl-hPj6Y'
-bot=telebot.Telebot(token)
-@bot.message_handler(commands=['start'])
-def start_message(message):
-  bot.send_message(message.chat.id,"Привет ✌️ ")
-bot.infinity_poling()
+OpenBot = telebot.TeleBot('5554332230:AAFnh_HngpQesOHui_kEZGSjJ0Vcl-hPj6Y', parse_mode = None)
+
+@OpenBot.message_handler(commands = ['start'])
+def start(message):
+    mess =f'Привет, <b>{message.from_user.first_name} </b> '
+    OpenBot.send_message(message.chat.id, mess, parse_mode='html')
+OpenBot.polling(non_stop=True)
